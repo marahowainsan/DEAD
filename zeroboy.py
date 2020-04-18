5cb601a9740a4cd389f0dd2cff95a5edb6cde4ab
marahowainsan <63833817+marahowainsan@users.noreply.github.com>
2020-04-18 07:19:55 +0500

    Update zeroboy.py

--- zeroboy.py
+++ zeroboy.py
@@ -49,9 +49,9 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•THE DEAD USER-----•◈•
+|__\~~) •||•KASHIF  - BALOCB---------------•◈•
+|__(-----\  •◈•------MARA HOWA INSAN---•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mMarahowainsan
+\033[1;96mInstagram \033[1;93m: \033[1;92m
+\033[1;96mFacebook  \033[1;93m: \033[1;92m Kashifbaloch78
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/marahowainsan/DEAD
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "dead"
+CorrectPassword = "dead"
 
 loop = 'true'
 while (loop == 'true'):
